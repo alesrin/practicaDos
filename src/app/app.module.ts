abb5c559979c9190b8cbb082960b6ae36e5bec09
javiasanchi <javi.ssal@hotmail.es>
2023-07-19 19:42:56 +0200

    Grupo 3 - app.module actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TareasModule } from './tareas/tareas.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './tareas/task-list/task-list.component';
+import { TaskDetailComponent } from './tareas/task-detail/task-detail.component';
+import { TaskFormComponent } from './tareas/task-form/task-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskDetailComponent,
+    TaskFormComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TareasModule
+    TareasModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
