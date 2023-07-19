5e645857e0d13f58a0d66fd3df4a3d1eb5f2ff80
javiasanchi <javi.ssal@hotmail.es>
2023-07-19 19:43:49 +0200

    Grupo 3 - tasklist.component actualizado

--- src/app/tareas/task-list/task-list.component.ts
+++ src/app/tareas/task-list/task-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TareaService } from 'src/app/tarea.service';
+import { Tarea } from 'src/app/tarea.model';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
-
+export class TaskListComponent implements OnInit {
+  tareas: Tarea[] = [];
+  constructor(private tareaService:
+    TareaService) { }
+  
+  ngOnInit() {
+    this.actualizarTareas();
+  }
+  actualizarTareas() {
+    this.tareas = this.tareaService.getTareas();
+  }
+  eliminarTarea(id: number) {
+    this.tareaService.eliminarTarea(id);
+    this.actualizarTareas();
+  }
+  editarTarea(tarea: Tarea) {
+    this.tareaService.setTareaParaEditar(tarea);
+  }
 }
