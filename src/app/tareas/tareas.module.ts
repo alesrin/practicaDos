334d383a253dccb4cdf0578e198ab95c7d66aad2
javiasanchi <javi.ssal@hotmail.es>
2023-07-19 09:34:15 +0200

    Actualizar practicaDos

---
+++ src/app/tareas/tareas.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+
+
+@NgModule({
+  declarations: [
+    TaskListComponent,
+    TaskFormComponent,
+    TaskDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TaskListComponent,
+    TaskFormComponent,
+    TaskDetailComponent
+  ]
+})
+export class TareasModule { }
